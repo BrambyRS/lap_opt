ca7f37b7f6232c94c8121b7b48373458fb81d5fe
Ranbir Singh <ranbir1998@gmail.com>
2025-11-06 20:06:55 +0000

    Add track definition and straight track example

--- pytrack/main.py
+++ pytrack/main.py
@@ -1,6 +1,18 @@
-def main():
-    print("Hello from pytrack!")
+import os
 
+from track import Track
+
+def generate_straight(length: float) -> Track:
+    # Generate a straight track with the given length
+    name = f"{length:.0f} m Straight"
+    track = Track(name, [(0, 0), (length, 0)])
+    return track
+
+def main():
+    pytrack_dir = os.path.dirname(os.path.abspath(__file__))
+
+    track = generate_straight(100)
+    track.write_trk(os.path.join(os.path.dirname(pytrack_dir), "tracks", "straight.trk"))
 
 if __name__ == "__main__":
     main()
