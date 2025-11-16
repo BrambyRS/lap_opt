0c70d3cb9f42dbd259208886330449e9ac932181
Ranbir Singh <ranbir1998@gmail.com>
2025-11-16 21:46:21 +0000

    Update example in main.rs

--- src/main.rs
+++ src/main.rs
@@ -29,13 +29,32 @@ fn main() {
         writeln!(file, "{},{},{},{},{}", t, x[0], x[1], x[2], x[3]).unwrap();
     }
 
-    let test_track =
+    let test_track: track::Track =
         track::Track::read_from_file("/Users/rsingh/Repos/lap_opt/tracks/gbg_city_arena.trk");
+    println!("{}", test_track);
 
-    let points: Vec<(f64, f64, f64)> = test_track.points();
-    let mut track_csv = File::create("track_points.csv").unwrap();
-    writeln!(track_csv, "x,y,width").unwrap();
-    for (x, y, width) in points {
-        writeln!(track_csv, "{},{},{}", x, y, width).unwrap();
+    let n_frames: usize = 1000;
+    let ds: f64 = test_track.length() / (n_frames as f64);
+    let mut s_lap_q: Vec<f64> = Vec::with_capacity(n_frames);
+    for i in 0..n_frames {
+        s_lap_q.push(i as f64 * ds);
+    }
+
+    let track_frames: Box<Vec<track::TrackFrame>> = test_track.discretise(s_lap_q);
+
+    let csv_path: &str = "track_points.csv";
+    let mut track_file: File = File::create(csv_path).unwrap();
+    writeln!(track_file, "xc,yc,xl,yl,xr,yr").unwrap();
+    for frame in track_frames.iter() {
+        let (xc, yc) = frame.position();
+        let (nx, ny) = frame.lateral();
+        let width: f64 = frame.width();
+
+        let xl: f64 = xc + (width / 2.0) * nx;
+        let yl: f64 = yc + (width / 2.0) * ny;
+        let xr: f64 = xc - (width / 2.0) * nx;
+        let yr: f64 = yc - (width / 2.0) * ny;
+
+        writeln!(track_file, "{xc},{yc},{xl},{yl},{xr},{yr}").unwrap();
     }
 }
