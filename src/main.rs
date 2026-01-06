0a113c67862024073cbe2a8fa35a5e8af6e1cff6
Ranbir Singh <ranbir1998@gmail.com>
2026-01-06 14:11:11 +0100

    Extend main.rs up to discretisaion

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ mod model;
 mod track;
 
 // Available model types
-#[derive(Debug, Clone, ValueEnum)]
+#[derive(Debug, Clone, ValueEnum, PartialEq)]
 enum ModelType {
     PointMass,
 }
@@ -43,4 +43,43 @@ fn main() {
 
     logger.log_info(&format!("Model: {:?}", args.model));
     logger.log_info(&format!("Track: {:?}", args.track));
+
+    let track: track::Track = match args.track {
+        TrackType::DoubleLaneChange => track::Track::double_lane_change(),
+        TrackType::Straight => track::Track::straight(1.0, 2.0),
+    };
+
+    if args.model == ModelType::PointMass {
+        let model: model::point_mass::PointMass =
+            model::point_mass::PointMass::new("Point Mass", 1500.0);
+    }
+
+    let n_segments: usize = 1;
+    let n_quadrature: usize = 3;
+    let nc: usize = n_segments * n_quadrature + 1; // Number of total collocation nodes including first point
+    logger.log_info(&format!("Discretising track into {n_segments} segments using {n_quadrature} quadrature points per segment."));
+    let flgr: collocation::FLGR = match collocation::FLGR::new(n_quadrature) {
+        Some(colloc) => colloc,
+        None => {
+            logger.log_error(&format!(
+                "Failed to create FLGR collocation of degree {n_quadrature}"
+            ));
+            return;
+        }
+    };
+
+    let mut s: Vec<f64> = Vec::<f64>::with_capacity(nc);
+    let l: f64 = track.length();
+    s.push(0.0); // First node at s=0
+                 // All other according to collocation
+    for i in 0..n_segments {
+        let s0: f64 = (i as f64) / (n_segments as f64) * l;
+        let s1: f64 = (i as f64 + 1.0) / (n_segments as f64) * l;
+        let sq: Vec<f64> = flgr.map_nodes(s0, s1);
+        for &node in &sq {
+            s.push(node);
+        }
+    }
+    logger.log_debug(&format!("Collocation nodes s: {:?}", s));
+    let discrete_track: Box<Vec<track::TrackFrame>> = track.discretise(s);
 }
