92c0b6afb033c44ab8bb1f76c2e8c5058c262cc0
Ranbir Singh <ranbir1998@gmail.com>
2026-01-06 12:23:29 +0100

    Add argument parsing to main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use clap::{Parser, ValueEnum};
 use simulation_toolbox::erk::ExplicitRK;
 use std::fs::File;
 use std::io::Write;
@@ -7,56 +8,36 @@ mod ipopt;
 mod model;
 mod track;
 
+// Available model types
+#[derive(Debug, Clone, ValueEnum)]
+enum ModelType {
+    PointMass,
+}
+
+#[derive(Debug, Clone, ValueEnum)]
+enum TrackType {
+    DoubleLaneChange,
+    Straight,
+}
+
+// APEX is a lap time optimisation tool using direct collocation.
+//
+// APEX solves the optimal control problem of minimising lap time given a vehicle model and track using a direct collocation framework. The resulting nonlinear program is solved using IPOPT with the MA27 solver.
+#[derive(Parser, Debug)]
+#[command(version)]
+struct Args {
+    // Model to simulate
+    #[arg(short, long)]
+    model: ModelType,
+
+    // Path to the track file
+    #[arg(short, long)]
+    track: TrackType,
+}
+
 fn main() {
-    let solver: ExplicitRK = ExplicitRK::rk4();
-    let point_mass: model::point_mass::PointMass =
-        model::point_mass::PointMass::new("PointMass1", 1.0);
+    let args = Args::parse();
 
-    let x0: Vec<f64> = vec![0.0, 0.0, 0.0, 0.0]; // Initial state: [x, y, vx, vy]
-    let u: Vec<f64> = vec![1.0, 0.0]; // Control input: [Fx, Fy]
-    let t0: f64 = 0.0;
-    let tf: f64 = 10.0;
-    let dt: f64 = 0.1;
-    let mut x: Vec<f64> = x0.clone();
-    let mut t: f64 = t0;
-
-    let mut file: File = File::create("sim_out.csv").unwrap();
-    writeln!(file, "time,x,y,vx,vy").unwrap();
-
-    while t < tf {
-        // Update the state using the solver
-        x = solver.step(&point_mass, &x, &u, t, dt);
-        t += dt;
-
-        writeln!(file, "{},{},{},{},{}", t, x[0], x[1], x[2], x[3]).unwrap();
-    }
-
-    let test_track: track::Track =
-        track::Track::read_from_file("/Users/rsingh/Repos/lap_opt/tracks/gbg_city_arena.trk");
-    println!("{}", test_track);
-
-    let n_frames: usize = 1000;
-    let ds: f64 = test_track.length() / (n_frames as f64);
-    let mut s_lap_q: Vec<f64> = Vec::with_capacity(n_frames);
-    for i in 0..n_frames {
-        s_lap_q.push(i as f64 * ds);
-    }
-
-    let track_frames: Box<Vec<track::TrackFrame>> = test_track.discretise(s_lap_q);
-
-    let csv_path: &str = "track_points.csv";
-    let mut track_file: File = File::create(csv_path).unwrap();
-    writeln!(track_file, "xc,yc,xl,yl,xr,yr").unwrap();
-    for frame in track_frames.iter() {
-        let (xc, yc) = frame.position();
-        let (nx, ny) = frame.lateral();
-        let width: f64 = frame.width();
-
-        let xl: f64 = xc + (width / 2.0) * nx;
-        let yl: f64 = yc + (width / 2.0) * ny;
-        let xr: f64 = xc - (width / 2.0) * nx;
-        let yr: f64 = yc - (width / 2.0) * ny;
-
-        writeln!(track_file, "{xc},{yc},{xl},{yl},{xr},{yr}").unwrap();
-    }
+    println!("Model: {:?}", args.model);
+    println!("Track: {:?}", args.track);
 }
